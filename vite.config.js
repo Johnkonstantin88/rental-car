3e91e6b30a183198ef08352524ae6858d55b0226
John_Konstantin <overdose.ut@gmail.com>
2025-03-14 23:16:18 +0200

    quick fix

--- vite.config.js
+++ vite.config.js
@@ -9,32 +9,32 @@ const DEFAULT_OPTIONS = {
   includePublic: true,
   logStats: true,
   ansiColors: true,
-  // svg: {
-  //   multipass: true,
-  //   plugins: [
-  //     {
-  //       name: 'preset-default',
-  //       params: {
-  //         overrides: {
-  //           cleanupNumericValues: false,
-  //           removeViewBox: false,
-  //         },
-  //         cleanupIDs: {
-  //           minify: false,
-  //           remove: false,
-  //         },
-  //         convertPathData: false,
-  //       },
-  //     },
-  //     'sortAttrs',
-  //     {
-  //       name: 'addAttributesToSVGElement',
-  //       params: {
-  //         attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
-  //       },
-  //     },
-  //   ],
-  // },
+  svg: {
+    multipass: true,
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            cleanupNumericValues: false,
+            removeViewBox: false,
+          },
+          cleanupIDs: {
+            minify: false,
+            remove: false,
+          },
+          convertPathData: false,
+        },
+      },
+      'sortAttrs',
+      {
+        name: 'addAttributesToSVGElement',
+        params: {
+          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+        },
+      },
+    ],
+  },
   png: {
     quality: 75,
   },
